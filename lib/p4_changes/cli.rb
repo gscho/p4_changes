a9794d96882e100665aea3864298f8ec8dec4663
Greg Schofield <gregs@indellient.com>
2017-08-04 14:48:48 -0400

    Updated CLI to use real perforce command

--- lib/p4_changes/cli.rb
+++ lib/p4_changes/cli.rb
@@ -4,11 +4,22 @@ module P4Changes
   class CLI < Thor
     package_name 'p4_changes'
 
-    desc "files DEPOT_LOCATION", "list all changed files at a depot location"
+    desc 'files DEPOT_LOCATION', 'list all changed files at a depot location'
+    method_option :from, :default => ''
+    method_option :to, :default => '#head'
     def files(depot_location)
-      res = `git --version`.split( /\n/ )
+      res = `git --help`.split( /\n/ )
+      changelists = []
       res.each do |line|
-        puts line
+        changelist = line.split(' ')
+        changelists << changelist[1] unless changelist[1].nil?
+      end
+      files = []
+      changelists
+      .map { |num| `p4 -Ztag -F "%depotFile%" files @=#{num}`.split( /\n/ ) }
+      .flatten
+      .each do |path|
+        puts path
       end
     end
   end
